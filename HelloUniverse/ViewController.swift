944814662de1a5253ebc79d8f35725fde3e43fa6
Vinicius Meira <viniciusmeirabr@hotmail.com>
2023-02-19 11:00:45 -0300

    Initial commit

---
+++ HelloUniverse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloUniverse
+//
+//  Created by Vinicius Meira on 07/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
